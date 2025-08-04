3f963cf603cc5eefa322c763c054caa94a449a04
congoodrum <c_goodrum@outlook.com>
2025-08-04 11:13:58 +0100

    Add in the List chapter exercises

---
+++ Python/CodeDex/Exercises/5 -Lists/todoList.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+todoList = ['Get Quarters', 'Do laundry', 'Take a walk', 'Get a haircut', 'Make some tea', 'Complete Lists chapter', 'Call mom', 'Watch My Hero Academia']
+
+print(todoList[0]) # Print the first item
+print(todoList[1]) # Print the second item
+print(todoList[2:3]) # Slice the 3rd and 4th items
+print(todoList[2:4]) # Slice the 3rd, 4th and 5th items
