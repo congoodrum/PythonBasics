d7ff708a98cc6dd755bc4b4fd9f7aa3672467630
congoodrum <c_goodrum@outlook.com>
2025-08-04 16:33:11 +0100

    Add the Challenges from the Functions chapter

---
+++ Python/CodeDex/Exercises/6 - Functions/Challenges/dog_years.py
@@ -0,0 +1,7 @@
+def dog_years(name, age):
+    human_age = age * 7
+    return f"{name} is {human_age} years old in human years."
+
+print(dog_years('Landon', 3))
+print(dog_years('Red Bean', 6))
+print(dog_years('Cooper', 2))
