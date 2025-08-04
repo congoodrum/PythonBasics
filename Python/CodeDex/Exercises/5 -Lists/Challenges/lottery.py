ef48e4afde353875eadefcad1111fd8a173b55dd
congoodrum <c_goodrum@outlook.com>
2025-08-04 16:11:57 +0100

    Added the Challenges from Loops chapter

---
+++ Python/CodeDex/Exercises/5 -Lists/Challenges/lottery.py
@@ -0,0 +1,15 @@
+# Write code below 💖
+import random
+
+my_numbers = []
+winning_numbers = []
+
+for i in range(0, 5):
+  my_numbers.append(random.randint(1,69))
+  winning_numbers.append(random.randint(1,69))
+
+my_numbers.append(random.randint(1,26))
+winning_numbers.append(random.randint(1,26))
+
+print(my_numbers)
+print(winning_numbers)
