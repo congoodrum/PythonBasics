3ec7240bdb36d5cb55f4270562d2b708c34b3185
congoodrum <c_goodrum@outlook.com>
2025-08-19 09:35:23 +0100

    Updated the folder structure to facilitate the new course. Added in files relating to python intermediate chapter 1

---
+++ Python/CodeDex/Intermediate Course/Exercises/tuples.py
@@ -0,0 +1,10 @@
+# tuples are immutable sequences, and cannot be changed
+
+belper = [53.0244, 1.4776]
+newcastle = [54.9783, 1.6178]
+birmingham = [52.4823, 1.8900]
+nottingham = [52.9540, 1.1550]
+
+all_locations = [belper, newcastle, birmingham, nottingham]
+
+print(all_locations)
