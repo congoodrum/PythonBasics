0a7ba7004bb94001c9877b6be7cc0db082d69385
congoodrum <c_goodrum@outlook.com>
2025-08-19 10:04:17 +0100

    Added in new folder for exercises under the new course and restrucrtured files. Creation of first exercise file in chapter 2

---
+++ Python/CodeDex/Intermediate Course/Exercises/2 - File I_O/diary_writer.py
@@ -0,0 +1,4 @@
+file = open('diary.txt', 'w')
+
+file.write("Entry 1: \n This is the first entry in my diary... another will soon follow." \
+"\n\nEntry 2: \n This is the second entry to the diary!")
