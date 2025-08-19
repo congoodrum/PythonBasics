5e47137ac22eeb6bb1b1cf568ebfbc9b52499b21
Connor Goodrum <37551147+congoodrum@users.noreply.github.com>
2025-08-19 09:36:07 +0100

    Intermediate data structures start (#6)
    * Add in gitignore file and roerbot file
    
    * Updated the folder structure to facilitate the new course. Added in files relating to python intermediate chapter 1

---
+++ Python/CodeDex/Intermediate Course/Exercises/dictionary.py
@@ -0,0 +1,4 @@
+# Dictionaries are unordered sequences of key-value pairs
+
+exhibit = {'Name': 'Oedipus and the Sphinx','Artist':'Gustave Moreau', 'period':'Paris 1826–1898 Paris', 'date':'1864'}
+print(exhibit)
---
+++ Python/CodeDex/Intermediate Course/Exercises/set.py
@@ -0,0 +1,7 @@
+# sets are unordered lists of unique items
+
+my_fruit_list = {'Apple', 'banana', 'Pear'}
+friend_fruit_list = {'Orange', 'Cherries', 'banana'}
+
+print(my_fruit_list.union(friend_fruit_list)) #print both sets combined
+print(my_fruit_list.intersection(friend_fruit_list)) #print oly the common itemsc
